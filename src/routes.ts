51566ea95065c072400ea0b99eec2dd7c33889f6
Renan Bertoldo <renan.rbertoldo@gmail.com>
2020-12-08 01:51:19 -0300

    feat: add prettier and fix lint

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,9 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response } from 'express';
 
 const routes = express.Router();
 
-routes.get("/", (request: Request, response: Response) => {
-  response.json({ message: "Hello World" });
+routes.get('/', (request: Request, response: Response) => {
+  response.json({ message: 'Hello World' });
 });
 
 export default routes;
